05500ea1942fb32848e75917413d570784f5338c
Warren <warrendsteinacker@gmail.com>
2025-12-09 15:15:37 -0500

    testing this code

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("m")
+const { request, response } = require('express')
+
+app = require('express')
+
+
+
+
+app.get('/', (request, response) => {
+    response.send(
+
+SubmitEvent = () => {
+    document.getElementById("1").value = "value changed"
+}
+
+<body style="background-color: darkblue;">
+
+<div 
+style="border: 2px solid black;">
+    <input id="1" type="text" value="enter"></input>
+    <button style="border: 2px solid red; padding: 5px;" onClick={SubmitEvent()}></button>
+</div>
+</body>
+    )
+} )
+
+app.listen(3000, () =>
+{
+    console.log( "m")
+})
