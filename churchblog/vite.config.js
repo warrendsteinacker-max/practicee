a276a4ba1b53d5ac95d13545447fc98b7185ca1d
Warren <warrendsteinacker@gmail.com>
2026-04-30 20:52:07 -0400

    makeing this website a pwa

--- churchblog/vite.config.js
+++ churchblog/vite.config.js
@@ -1,9 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/practicee/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'My Geo Track App',
+        short_name: 'GeoTrack',
+        description: 'Track my hours and location',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   envDir: '../'
 })
+
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/practicee/',
+//   plugins: [react()],
+//   envDir: '../'
+// })
